fd30d0dc44c33c7f53ce6c32d34b2918d560814f
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-05-21 10:40:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { hostname } from "node:os";
 
 import createBareServer from "@tomphttp/bare-server-node";
 import express from "express";
+
+const bare = createBareServer("/bare/");
 var app = express();
 //var server = require('http').createServer(app);//
 var port = process.env.PORT || 3000;
@@ -13,3 +15,45 @@ app.listen(port, function () {
 });
 
 app.use(express.static('public'));
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+server.listen({
+  port,
+});
+
