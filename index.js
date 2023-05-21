c638a5575d9a4a5f4e8a6105dc37aacc94e77041
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-05-21 10:41:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8080;
-
 server.on("listening", () => {
   const address = server.address();
 
