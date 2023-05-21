b1bd7bce77ad7e36449eaf96211b87c94a667dbf
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-05-21 10:30:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import { hostname } from "node:os";
 import createBareServer from "@tomphttp/bare-server-node";
 import express from "express";
 var app = express();
-var server = require('http').createServer(app);
+//var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
+app.listen(port, function () {
   console.log('----< ASAIGO >----\nListening at port %d!', port);
 });
 
