026c8bc161052db4405241c62349b1b219f0d65b
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-05-21 10:12:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var { uvPath } = require("@titanium");
+var { join } = require("node:path");
+var { hostname } = require("node:os");
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('----< ASAIGO >----\nListening at port %d!', port);
+});
+
+app.use(express.static('public'));
